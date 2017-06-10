4cbe2cd1108d2c6d80144e66e2ed7de19cd0b7fa
erose357 <erose357@gmail.com>
2017-06-10 09:23:51 -0600

    Fix invoice item test

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -5,57 +5,67 @@ require 'pry'
 
 class InvoiceItemTest < Minitest::Test
 
-  def test_new_instance
-    iir = InvoiceItemRepository.new("./test/data/invoice_items_fixture.csv", self)
+  attr_reader :iir,
+              :ii
 
-    assert_instance_of InvoiceItem, iir.contents[1]
+
+  def setup
+    @iir = InvoiceItemRepository.new("./test/data/invoice_items_fixture.csv", self)
+
+    @ii = InvoiceItem.new({ id:          "10",
+                           item_id:     "263523644",
+                           invoice_id:  "2",
+                           quantity:    "4",
+                           unit_price:  "1859",
+                           created_at:  "2012-03-27 14:54:09 UTC",
+                           updated_at:  "2012-03-27 14:54:09 UTC" }, @iir)
+  end
+
+  def test_new_instance
+
+    assert_instance_of InvoiceItem, ii
   end
 
   def test_return_id_integer
-    iir = InvoiceItemRepository.new("./test/data/invoice_items_fixture.csv", self)
 
-    assert_equal 13, iir.contents[13].id
+    assert_equal 10, ii.id
   end
 
   def test_return_invoice_id
-    iir = InvoiceItemRepository.new("./test/data/invoice_items_fixture.csv", self)
 
-    assert_equal 5, iir.contents[24].invoice_id
+    assert_equal 2, ii.invoice_id
   end
 
   def test_return_item_id
-    iir = InvoiceItemRepository.new("./test/data/invoice_items_fixture.csv", self)
 
-    assert_equal 263537372, iir.contents[41].item_id
+    assert_equal 263523644, ii.item_id
   end
 
   def test_return_quantity
-    iir = InvoiceItemRepository.new("./test/data/invoice_items_fixture.csv", self)
 
-    assert_equal 5, iir.contents[1].quantity
+    assert_equal 4, ii.quantity
   end
 
   def test_return_unit_price
-    iir = InvoiceItemRepository.new("./test/data/invoice_items_fixture.csv", self)
 
-    assert_equal 42.64, iir.contents[14].unit_price.to_f
+    assert_equal 18.59, ii.unit_price
+    assert_instance_of BigDecimal, ii.unit_price
   end
 
   def test_return_created_at
-    iir = InvoiceItemRepository.new("./test/data/invoice_items_fixture.csv", self)
 
-    assert_equal "2012-03-27 14:54:09 UTC", iir.contents[1].created_at.to_s
+    assert_equal "2012-03-27 14:54:09 UTC", ii.created_at.to_s
+    assert_instance_of Time, ii.created_at
   end
 
   def test_return_updated_at
-    iir = InvoiceItemRepository.new("./test/data/invoice_items_fixture.csv", self)
 
-    assert_equal "2012-03-27 14:54:10 UTC", iir.contents[41].updated_at.to_s
+    assert_equal "2012-03-27 14:54:09 UTC", ii.updated_at.to_s
+    assert_instance_of Time, ii.updated_at
   end
 
   def test_unit_price_to_dollars
-    iir = InvoiceItemRepository.new("./test/data/invoice_items_fixture.csv", self)
 
-    assert_equal 707.83, iir.contents[41].unit_price_to_dollars(41)
+    assert_equal 707.83, ii.unit_price_to_dollars(41)
   end
 end
